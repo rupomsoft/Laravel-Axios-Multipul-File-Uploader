2354ee8f1a5a151ccb5f212b3234e5960def9969
rupomsoft <33941621+rupomsoft@users.noreply.github.com>
2020-05-04 20:43:27 +0600

    card margin added
    card margin added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 
 
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid mt-5">
     <div class="row">
             <div class="col-md-12">
                 <div class="card">
